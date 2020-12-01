2a303a5115e8d3fb91f3597fbf7ec0965ec96c3e
sudhir shukla <sudhir.shukla@in2ittech.com>
2020-12-01 17:38:57 +0530

    Rule configuration

--- src/app/modules_/case/case.component.ts
+++ src/app/modules_/case/case.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaseComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  
+  }
+
+  showDDNMS(event) {
+    setTimeout(function () {
+      const element_: Element = (event.target as Element);
+      const elementDD: Element = element_.nextElementSibling;
+      const existingClass = elementDD.getAttribute('class');
+      const toggleClass = (existingClass.indexOf('show') > -1) ? existingClass.replace('show', '').trim() : existingClass + ' show';
+      //obj_['isListenOnBlur'] = (toggleClass.indexOf('show') > -1);
+      elementDD.setAttribute('class', toggleClass);
+    }, 100);
+  }
+
+ 
 
   ngOnInit() {
+    
   }
 
 }
--- src/app/modules_/dashboard/dashboard.module.ts
+++ src/app/modules_/dashboard/dashboard.module.ts
@@ -1,11 +1,14 @@
 import { DashboardRoutingModule } from './dashboard.routing.module';
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { LayoutsModule } from '../layouts/layouts.module'; 
 import { SharedModule } from 'src/app/shared_/shared.module';
 import { EpisodeComponent } from '../episode/episode.component';
 import { CaseComponent } from '../case/case.component';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { FormsModule } from '@angular/forms';
 // import { MapulaModule } from '../mapula/mapula.module';
 
 // import {DragDropModule} from '@angular/cdk/drag-drop';
@@ -15,9 +18,11 @@ import { CaseComponent } from '../case/case.component';
     SharedModule,
     CommonModule,
     DashboardRoutingModule,
-    LayoutsModule
+    LayoutsModule,
+    FormsModule,
     // MapulaModule,
     // DragDropModule
+    NgMultiSelectDropDownModule.forRoot()
   ],
   declarations: [DashboardComponent,EpisodeComponent,CaseComponent]
 })
