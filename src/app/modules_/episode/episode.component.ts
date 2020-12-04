716ee91220de069f7dee6c1da01353f601b2bd6b
arjun-singh-in2it <arjun.singh@in2ittech.com>
2020-12-04 14:08:55 +0530

    episode configuration binding

--- src/app/modules_/episode/episode.component.ts
+++ src/app/modules_/episode/episode.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EpisodeService } from './episode.services';
 
 @Component({
   selector: 'cats-episode',
@@ -7,9 +8,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EpisodeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private episodeService:EpisodeService) { }
+alarmList;
+operatorList;
+filterList;
+nmsList;
+episodeModel={
+  "name":"",
+  "description":"",
+  "selectedNams":"",
+  "selectedAlarm":"",
+  "filterMapping":[{'filter':"",'operator':"",'value':""}],
+  "notificationMapping":[{'type':"",'value':""}],
+  "subject":"",
+  "body":""
+}
   ngOnInit() {
+    this.episodeService.getAlarmList().subscribe((res)=>{
+      if(res['status']){
+        this.alarmList=res['data'];
+      }
+    });
+    this.episodeService.getOperatorList().subscribe((res)=>{
+      if(res['status']){
+        this.operatorList=res['data'];
+      }
+    });
+    this.episodeService.getFilterList().subscribe((res)=>{
+      if(res['status']){
+        this.filterList=res['data'];
+      }
+    });
+    this.episodeService.getInputSourceList().subscribe((res)=>{
+      if(res['status']){
+        this.nmsList=res['data'];
+      }
+    });
   }
 
+  addFilter(){
+    this.episodeModel.filterMapping.push({'filter':"",'operator':"",'value':""});
+  }
+  removeFilter(i){
+    this.episodeModel.filterMapping.splice(i,1);
+  }
+  addNotification(){
+    this.episodeModel.notificationMapping.push({'type':"",'value':""});
+  }
+  removeNotification(i){
+    this.episodeModel.notificationMapping.splice(i,1);
+  }
+  append(toText,whatText){
+   if(toText=="subject"){
+     if(whatText=="time"){
+      this.episodeModel.subject=this.episodeModel.subject+" $time "
+     }else if(whatText=="host"){
+      this.episodeModel.subject=this.episodeModel.subject+" $host "
+    }
+   }else{
+      if(whatText=="time"){
+        this.episodeModel.body=this.episodeModel.body+" $time "
+      }else if(whatText=="host"){
+        this.episodeModel.body=this.episodeModel.body+" $host "
+      }else if(whatText=="signature"){
+        this.episodeModel.body=this.episodeModel.body+" $signature "
+      }
+   }
+  }
 }
---
+++ src/app/modules_/episode/episode.services.ts
@@ -0,0 +1,46 @@
+
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { BehaviorSubject } from 'rxjs';
+
+import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { UserService } from 'src/app/services_/user.services';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({ providedIn: 'root' })
+export class EpisodeService {
+
+
+
+    constructor(private http: HttpClient, private userService: UserService
+    ) { }
+
+
+    public getAlarmList() { 
+            const url = environment._WEBGATEWAY_BASIC_URL_ + 'menu/kpi/get_alarm_list'; 
+            return this.http.post(url, {}).map(res => <any>res);
+        }
+        public getOperatorList() { 
+            const url = environment._WEBGATEWAY_BASIC_URL_ + 'menu/kpi/get_operator_list'; 
+            return this.http.post(url, {}).map(res => <any>res);
+        }
+        public getFilterList() { 
+            const url = environment._WEBGATEWAY_BASIC_URL_ + 'menu/kpi/get_filter_list'; 
+            return this.http.post(url, {}).map(res => <any>res);
+        }
+/**
+   *
+   * to get all input source list
+   *
+   */
+  getInputSourceList() {
+    const URL = environment._WEBGATEWAY_BASIC_URL_ + 'menu/kpi/get_input_source_list';
+    // const URL = 'https://run.mocky.io/v3/832984eb-82f4-4d35-a400-5507cbe3f481';
+    return this.http.post(URL, {}).map(res => <any>res);
+  }
+         
+
+}
